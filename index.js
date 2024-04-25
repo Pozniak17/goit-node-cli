dd2a013f5a8be59095b172f90fb1a0b2375f1359
Yevhenii Pozniak <jekamanu@gmail.com>
2024-04-25 14:24:25 +0300

    update

---
+++ index.js
@@ -0,0 +1,49 @@
+const contacts = require("./contacts");
+// import { program } from "commander";
+
+// program
+//   .option("-a, --action <type>", "choose action")
+//   .option("-i, --id <type>", "user id")
+//   .option("-n, --name <type>", "user name")
+//   .option("-e, --email <type>", "user email")
+//   .option("-p, --phone <type>", "user phone");
+
+// program.parse();
+
+// const options = program.opts();
+
+// TODO: рефакторити
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const oneContacts = await contacts.getContactById(id);
+      console.log(oneContacts);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    // case "remove":
+    //   // ... id
+    //   break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// invokeAction({ action: "list" });
+// invokeAction({ action: "get", id: "AeHIrLTr6JkxGE6SN-0Rw" });
+invokeAction({
+  action: "add",
+  name: "Glocktar",
+  email: "Soco@gmail.com",
+  phone: "(000) 451-7038",
+});
